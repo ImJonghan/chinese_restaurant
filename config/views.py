7d527726ff44f80453072996371fd2a67c078695
ImJonghan <ijhan21c@naver.com>
2024-04-17 10:39:01 +0900

    auth

--- config/views.py
+++ config/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView, TemplateView
+from django.urls import reverse_lazy
 def index(request):
     return render(request, 'home.html')
+
+class UserCreateView(CreateView):
+    template_name='registration/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('register_done')
+
+class UserCreateDoneTV(TemplateView):
+    template_name='registration/register_done.html'
